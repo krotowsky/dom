15d5663cdd3556176e506ec9da57c0632c0a4739
pawel <p.krotowski@topmall.pl>
2018-02-22 00:35:14 +0100

    Category

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,7 +19,7 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('default/home_section.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
@@ -66,7 +66,8 @@ class DefaultController extends Controller
             ->find($transactionId);
 
         return new Response(
-            $categoryName = $transaction->getCategory()->getName()
+            $categoryName = $transaction->getCategory()->getName(). '<bar>'.
+                $categoryName =$transaction>getU
         );
     }
 
